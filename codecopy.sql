aa4295e05e21325c8b7853c159db5522d0fe0056
Draie <110666345+Draie@users.noreply.github.com>
2023-08-15 21:42:49 +0200

    Write first table
    Change UML

---
+++ codecopy.sql
@@ -0,0 +1,57 @@
+CREATE TABLE cinema (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, adresse VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB;
+        CREATE TABLE salle (id INT AUTO_INCREMENT NOT NULL, relation_id INT NOT NULL, nb_place INT NOT NULL, INDEX IDX_4E977E5C3256915B (relation_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB';
+        CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB';
+        ALTER TABLE salle ADD CONSTRAINT FK_4E977E5C3256915B FOREIGN KEY (relation_id) REFERENCES cinema (id);
+
+
+        // Création de la base de donnée //
+
+        CREATE DATABASE cinedb;
+
+        //Création classes,entités //
+        CREATE TABLE cinema(
+        id INT AUTO_INCREMENT NOT NULL,
+        nom VARCHAR(255) NOT NULL,
+        Adresse VARCHAR NOT(255),
+                        // Création Accès au nombre de place dans une salle de cinéma //
+        CONSTRAINT FOREIGN KEY (nbPlace_id) REFERENCES salle (id);
+                        
+
+        );
+      
+            CREATE TABLE salle(
+          nbPlace INT NOT NULL DEFAULT CHECK(nbPlace <= 70), 
+          salleCounter INT AUTO_INCREMENT NOT NULL ,
+                   //Creation Accès au information du cinéma//
+                CONSTRAINT  FOREIGN KEY (cinema_id) REFERENCES cinema (id);
+
+
+
+            );
+
+            CREATE TABLE client(
+                id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY;
+                nom VARCHAR(50);
+                
+            );
+
+            CREATE TABLE Tarif(
+                id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+                Price INT NOT NULL,
+
+            );
+     
+     CREATE TABLE Film (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(300) NOT NULL,
+        Actor VARCHAR(300) DEFAULT Values,
+        Time INT CHECK (Time <=5),
+     );
+
+     CREATE TABLE reservation(
+        getClient CONSTRAINT FOREIGN KEY (client_id),
+        getPrice CONSTRAINT FOREIGN KEY (tarif_id),
+        getCinema CONSTRAINT FOREIGN KEY (cinema_id),
+        getFilm CONSTRAINT FOREIGN KEY(film_id),
+        
+     );
